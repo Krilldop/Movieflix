0973efc743426f14ce9e81e64ff00d7839fc050a
Daniel Trindade <daniptrindade3@gmail.com>
2024-01-14 01:57:49 +0000

    Front-end Movies/Error Concluido
    Front-end dos arquivos movies.php e error.php concluídos.

--- functions.php
+++ functions.php
@@ -21,18 +21,19 @@ if (isset($_POST["login"])) {
     exit();
   }
   else if ($checkPwd === true) {*/
-    session_start();
-    $_SESSION["idutilizador"] = $emailExists["idutilizador"];
-    header("Location: mainpage.php");
-    exit();
-  }
+  session_start();
+  $_SESSION["idutilizador"] = $emailExists["idutilizador"];
+  header("Location: mainpage.php");
+  exit();
+}
 //}
 else {
   header("Location: mainpage.php");
   exit();
 }
 
-function emailExists($conn, $email) {
+function emailExists($conn, $email)
+{
   $sql = "SELECT * FROM utilizadores WHERE email = ?;";
   $stmt = mysqli_stmt_init($conn);
   if (!mysqli_stmt_prepare($stmt, $sql)) {
@@ -47,11 +48,43 @@ function emailExists($conn, $email) {
 
   if ($row = mysqli_fetch_assoc($resultData)) {
     return $row;
-  }
-  else {
+  } else {
     $result = false;
     return $result;
   }
   mysqli_stmt_close($stmt);
 }
+
+if (isset($_POST['register'])) {
+
+  $username = $_POST["username"];
+  $email = $_POST["email"];
+  $password = $_POST["password"];
+
+  if (emailexist($conn, $email)) {
+    echo "<script>alert('O email inserido pertence a um utilizador'); window.location.href = 'register.php';</script>";
+    exit();
+  }
+
+  $sql = "INSERT INTO utilizadores (utilizador, email, password) VALUES (?, ?, ?)";
+  $stmt = mysqli_stmt_init($conn);
+
+  if (!mysqli_stmt_prepare($stmt, $sql)) {
+    echo "Error: " . mysqli_stmt_error($stmt);
+    header("Location: dbproblems.php");
+    exit();
+  } else {
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPassword);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    echo "<script>alert('Registo feito com sucesso!'); window.location.href = 'login.php';</script>";
+    exit();
+  }
+} else {
+  header("Location: error.php");
+  exit();
+}
+
 ?>
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,26 @@
+function filterBtn(button) {
+  changeColor(button);
+  filterMovies(button);
+}
+
 function changeColor(button) {
   let buttons = document.querySelectorAll(".genrebtn");
   buttons.forEach(function (btn) {
     btn.classList.remove("selected");
   });
   button.classList.add("selected");
+}
+
+function filterMovies(button) {
+  const filterValue = button.getAttribute("data-filter");
+  const movieCells = document.querySelectorAll('.moviecol');
+
+  movieCells.forEach(cell => {
+    const cellFilter = cell.getAttribute("data-filter");
+    if (filterValue === "all" || filterValue === cellFilter) {
+      cell.style.display = "";
+    } else {
+      cell.style.setProperty("display", "none", "important")
+    }
+  });
 }
