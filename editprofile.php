9e5baf926e90718763313940e16c9023b9dcab7b
Daniel Trindade <daniptrindade3@gmail.com>
2024-01-14 05:14:41 +0000

    Front-end Editprofile concluido
    Front-end do arquivo editprofile.php concluido

--- editprofile.php
+++ editprofile.php
@@ -14,6 +14,39 @@
 <body class="d-flex flex-column min-vh-100">
   <?php include_once "navbar_postlogin.php" ?>
 
+  <div class="container m-auto">
+    <div class="editprofilecontainer m-auto d-flex justify-content-center align-items-center">
+
+      <form action="profile.php" method="post">
+
+        <h4 class="display-4 fs-1 text-white">Editar Perfil</h4><br>
+        <div class="mb-3">
+          <label class="form-label text-white">Username</label>
+          <input type="text" class="form-control" name="username" value="Username">
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label text-white">Email</label>
+          <input type="text" class="form-control" name="email" value="example@example.com">
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label text-white">Password</label>
+          <input type="password" class="form-control" name="password" value="">
+        </div>
+
+        <div class="mb-3">
+          <label class="form-label text-white">Profile Picture</label>
+          <input type="file" class="form-control" name="userimage">
+          <img class="userpic rounded-circle mt-3" src="assets/users/user_1.jpeg" alt="">
+          <input type="text" hidden="hidden" name="olduserimage" value="userx">
+        </div>
+
+        <button type="submit" class="btn editprofilebtn text-white fw-bold mb-2">Atualizar</button>
+      </form>
+    </div>
+  </div>
+
   <?php include_once "footer.php" ?>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
