febe2e8ef96c0e4f157433c856fb19b5e313f098
Daniel Trindade <daniptrindade3@gmail.com>
2024-01-17 09:58:10 +0000

    Adicionada tab "Sobre" esquecida

--- navbar_prelogin.php
+++ navbar_prelogin.php
@@ -3,6 +3,11 @@
         <a class="navbar-brand" href="index.php">
             <img src="assets/brand-no-background.png" alt="Movieflix" width="210" height="60" />
         </a>
+        <ul class="navbar-nav col-md-6 justify-content-md-center">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="about.php">Sobre</a>
+            </li>
+        </ul>
 
         <div class="navbar d-md-flex" id="navbarsExample11">
             <div class="d-md-flex col-md-12 justify-content-md-end">
